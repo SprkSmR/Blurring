acb08bbf937fcc0c8bcb84aad9f40b43adc30812
SprkSmR <chitox_ji_armen@hotmail.com>
2024-04-19 13:57:13 -0600

    Progreso blur

--- Blur.c
+++ Blur.c
@@ -8,26 +8,48 @@ int main()
     outputImage = fopen("img2_E.bmp","wb");    //Imagen transformada
     long ancho;
     long alto;
+    int mask = 3;
     
     unsigned char r, g, b, p[2];               //Pixel
+    unsigned char r_temp, g_temp, b_temp;               //Pixel
+    unsigned char r_mean[mask*mask], g_mean[mask*mask], b_mean[mask*mask];
     unsigned char* ptr;
 
     unsigned char xx[54];
     int cuenta = 0;
+
+
     for(int i=0; i<54; i++) {
       xx[i] = fgetc(image);
       fputc(xx[i], outputImage);   //Copia cabecera a nueva imagen
     }
-    printf(" byte_1: %x\nbyte_2: %x\n byte_3: %x\n byte_4: %x\n", xx[18], xx[19], xx[20], xx[21]);
 
     ancho = (long)xx[20]*65536+(long)xx[19]*256+(long)xx[18];
     alto = (long)xx[24]*65536+(long)xx[23]*256+(long)xx[22];
     printf("ancho img %li\n",ancho);
     printf("largo img %li\n",alto);
 
-    ptr = (unsigned char*)malloc(alto*ancho*3*sizeof(unsigned char));
+    ptr = (unsigned char*)malloc((alto+(mask%2)*2)*(ancho+(mask%2)*2)*3*sizeof(unsigned char));
 
+    for (int i = 0; i < mask%2; i++){
+      for (int j = 0; j < (ancho+(mask%2)*2)*3; j+=3){
+        ptr[j] = 0;
+        ptr[j + 1] = 0;
+        ptr[j + 2] = 0;
+      }
+    }
+    
+    cuenta += (ancho+(mask%2)*2)*3*(mask%2);
+    
     while(!feof(image)){
+      for (int i = cuenta; i < cuenta + (mask%2)*3; i+=3){
+        ptr[i] = 0;
+        ptr[i + 1] = 0;
+        ptr[i + 2] = 0;
+      }
+
+      cuenta += (mask%2)*3;
+
       for (int i = 0; i < ancho*3 && !feof(image); i+=3){
         b = fgetc(image);
         g = fgetc(image);
@@ -38,23 +60,46 @@ int main()
         ptr[cuenta + 2] = r; //r
         cuenta += 3;
       }
-      
+
       if (!feof(image)){
         p[0] = fgetc(image);
         p[1] = fgetc(image);
-
-        ptr[cuenta] = p[0];
-        ptr[cuenta+1] = p[1];
-
-        cuenta += 2;
       }
-    }                                        //Grises
-    printf("%d\n",cuenta);
+      
+      for (int i = cuenta; i < cuenta + (mask%2)*3; i+=3){
+        ptr[i] = 0;
+        ptr[i + 1] = 0;
+        ptr[i + 2] = 0;
+      }
 
-    for (int i = 0; i < alto*ancho*3; i+=3) {
-      fputc(ptr[i], outputImage);
-      fputc(ptr[i+1], outputImage);
-      fputc(ptr[i+2], outputImage);
+      cuenta += (mask%2)*3;
+    }                                       //Grises
+
+    for (int i = 0; i < mask%2; i++){
+      for (int j = cuenta; j < cuenta + (ancho+(mask%2)*2)*3; j+=3){
+        ptr[j] = 0;
+        ptr[j + 1] = 0;
+        ptr[j + 2] = 0;
+      }
+    }
+
+    cuenta += (ancho+(mask%2)*2)*3*(mask%2);
+
+    for (int i = 0; i < alto; i++) {
+      for (int j = 0; j < ancho*3; j+=3) {
+        b_temp = ptr[((i+mask%2-1)*(ancho+(mask%2-1)*2)*3)+((j+mask%2)*3)];
+        b_mean[0] = b_temp;
+
+        
+        b_temp = ptr[((i+mask%2)*(ancho+(mask%2)*2)*3)+((j+mask%2)*3)];
+        g_temp = ptr[((i+mask%2)*(ancho+(mask%2)*2)*3)+((j+mask%2)*3)]; 
+        r_temp = ptr[((i+mask%2)*(ancho+(mask%2)*2)*3)+((j+mask%2)*3)]; 
+        fputc(ptr[((i+mask%2)*(ancho+(mask%2)*2)*3)+((j+mask%2)*3)], outputImage);
+        fputc(ptr[((i+mask%2)*(ancho+(mask%2)*2)*3)+((j+mask%2)*3)+1], outputImage);
+        fputc(ptr[((i+mask%2)*(ancho+(mask%2)*2)*3)+((j+mask%2)*3)+2], outputImage);
+      }
+      fputc(p[0], outputImage);
+      fputc(p[1], outputImage);
     }
 
     free(ptr);
